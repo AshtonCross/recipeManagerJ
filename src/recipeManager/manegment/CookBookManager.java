7685398c86fdd5ae024c1a58c01567b6258f7a3b
Ashton Cross <ashton.ccross@gmail.com>
2022-12-05 05:28:35 -0700

    initial commit

---
+++ src/recipeManager/manegment/CookBookManager.java
@@ -0,0 +1,18 @@
+package recipeManager.manegment;
+
+import recipeManager.bookData.CookBook;
+import java.io.File;
+
+public class CookBookManager {
+    
+    
+    public static void openCookBook(File cookBookFile) {
+        
+    }
+    
+    public static void writeToFile() {
+        
+    }
+    
+    
+}
